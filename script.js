5837e86515f4cb3b6d139fce4cddc2a5661d6171
Vladislav Bykov <therockshade@gmail.com>
2020-04-14 03:09:58 +0400

    feat and refactor: add task 7 and files for task 8

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 'use strict';
 
+let startBtn = document.getElementById('start'),
+
+		budgetValue = document.querySelector('.budget-value'),
+		dayBudgetValue = document.querySelector('.daybudget-value'),
+		levelValue = document.querySelector('.level-value'),
+		expensesValue = document.querySelector('.expenses-value'),
+		optionalExpensesValue = document.querySelector('.optionalexpenses-value'),
+		incomeValue = document.querySelector('.income-value'),
+		monthSavingsValue = document.querySelector('.monthsavings-value'),
+		yearSavingsValue = document.querySelector('.yearsavings-value'),
+
+		expensesItems = document.querySelectorAll('.expenses-item'),
+
+		buttons = document.querySelectorAll('button'),
+		expensesItemBtn = buttons[0],
+		optionalExpensesBtn = buttons[1],
+		countBudgetBtn = buttons[2],
+
+		optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+
+		income = document.querySelector('#income'),
+		savings = document.querySelector('#savings'),
+		sum = document.querySelector('#sum'),
+		percent = document.querySelector('#percent'),
+		yearValue = document.querySelector('.year-value'),
+		monthValue = document.querySelector('.month-value'),
+		dayValue = document.querySelector('.day-value');
+
 let money, time;
 
 function start() {
