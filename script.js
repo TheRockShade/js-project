28b890a8498cc93e9f96cb6987434eea1b00dd62
Vladislav Bykov <therockshade@gmail.com>
2020-04-15 15:13:25 +0400

    feat: add v1

--- script.js
+++ script.js
@@ -22,24 +22,126 @@ let startBtn = document.getElementById('start'),
 
 		income = document.querySelector('#income'),
 		savings = document.querySelector('#savings'),
-		sum = document.querySelector('#sum'),
-		percent = document.querySelector('#percent'),
+		sumItem = document.querySelector('#sum'),
+		percentItem = document.querySelector('#percent'),
 		yearValue = document.querySelector('.year-value'),
 		monthValue = document.querySelector('.month-value'),
 		dayValue = document.querySelector('.day-value');
 
 let money, time;
 
-function start() {
-	money = +prompt('Ваш бюджет на месяц?', '50000');
+expensesItemBtn.disabled = true;
+optionalExpensesBtn.disabled = true;
+countBudgetBtn.disabled = true;
+
+startBtn.addEventListener('click', function() {
 	time = prompt('Введите дату в формате YYYY-MM-DD', '1998-05-03');
+	money = +prompt('Ваш бюджет на месяц?', '50000');
 
 	while(isNaN(money) || money == '' || money == null) {
 		money = +prompt('Ваш бюджет на месяц?', '50000');
 	}
-}
+	appData.budget = money;
+	appData.timeData = time;
+	budgetValue.textContent = money.toFixed();
+	yearValue.value = new Date(Date.parse(time)).getFullYear();
+	monthValue.value = new Date(Date.parse(time)).getMonth() + 1;
+	dayValue.value = new Date(Date.parse(time)).getDate();
 
-start();
+	expensesItemBtn.disabled = false;
+	optionalExpensesBtn.disabled = false;
+	countBudgetBtn.disabled = false;
+});
+
+expensesItemBtn.addEventListener('click', function() {
+	let sum = 0;
+
+	for (let i = 0; i < expensesItems.length; i++) {
+		let requiredSpending = expensesItems[i].value;
+		let spending = expensesItems[++i].value;
+	
+		if(typeof(requiredSpending) === 'string' && typeof(requiredSpending) != null && typeof(spending) != null &&requiredSpending != '' && spending != '' && requiredSpending.length < 50) {
+			console.log('done');
+			appData.expenses[requiredSpending] = spending;
+			sum += +spending;
+		} else {
+			i = i - 1;
+		}
+	}
+	expensesValue.textContent = sum;
+});
+
+optionalExpensesBtn.addEventListener('click', function(){
+	for (let i = 0; i < optionalExpensesItem.length; i++) {
+		let opt = optionalExpensesItem[i].value;
+		appData.optionalExpenses[i] = opt;
+		optionalExpensesValue.textContent += appData.optionalExpenses[i] + ' ';
+	}
+});
+
+countBudgetBtn.addEventListener('click', function(){
+
+	if (appData.budget != undefined) {
+		appData.moneyPerDay = ((appData.budget - expensesValue.innerHTML) / 30).toFixed();
+		dayBudgetValue.textContent = appData.moneyPerDay;
+	
+		if(appData.moneyPerDay < 100) {
+			levelValue.textContent = 'Минимальный уровень достатка';
+		} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+			levelValue.textContent = 'Средний уровень достатка';
+		} else if (appData.moneyPerDay > 2000) {
+			levelValue.textContent = 'Высокий уровень достатка';
+		} else {
+			levelValue.textContent = 'Произошла ошибка';
+		}
+	} else {
+		dayBudgetValue.textContent = 'Произошла ошибка';
+	}
+});
+
+income.addEventListener('input', function(){
+	let items = income.value;
+
+	if (typeof(items) == 'string' || items != '' || typeof(items) != null) {
+		appData.income = items.split(', ');
+	}
+
+	incomeValue.textContent = appData.income;
+});
+
+savings.addEventListener('click', function(){
+	if (appData.savings == true) {
+		appData.savings = false;
+	} else {
+		appData.savings = true;
+	}
+});
+
+sumItem.addEventListener('input', function(){
+	if (appData.savings == true) {
+		let sum = +sumItem.value,
+				percent = +percentItem.value;
+
+		appData.monthIncome = (sum / 100 / 12 * percent).toFixed();
+		appData.yearIncome = (sum / 100 * percent).toFixed();
+
+		monthSavingsValue.textContent = appData.monthIncome;
+		yearSavingsValue.textContent = appData.yearIncome;
+	}
+});
+
+percentItem.addEventListener('input', function(){
+	if (appData.savings == true) {
+		let sum = +sumItem.value,
+				percent = +percentItem.value;
+
+		appData.monthIncome = (sum / 100 / 12 * percent).toFixed();
+		appData.yearIncome = (sum / 100 * percent).toFixed();
+
+		monthSavingsValue.textContent = appData.monthIncome;
+		yearSavingsValue.textContent = appData.yearIncome;
+	}
+});
 
 let appData = {
 	budget: money,
@@ -47,64 +149,5 @@ let appData = {
 	expenses: {},
 	optionalExpenses: {},
 	income: [],
-	savings: true,
+	savings: false,
 };
-
-function chooseExpenses() {
-	for (let i = 0; i < 2; i++) {
-		let requiredSpending = prompt('Введите обязательную статью расходов в этом месяце', '500');
-		let spending = prompt('Во сколько обойдется?', '10000');
-	
-		if(typeof(requiredSpending) === 'string' && typeof(requiredSpending) != null && typeof(spending) != null &&requiredSpending != '' && spending != '' && requiredSpending.length < 50) {
-			console.log('done');
-			appData.expenses[requiredSpending] = spending;
-		} else {
-			i = i - 1;
-		}
-	}
-}
-
-chooseExpenses();
-
-function detectDayBudget() {
-	appData.moneyPerDay = (appData.budget / 30).toFixed();
-	alert(`Ваш дневной бюджет: ${appData.moneyPerDay}`);
-}
-
-detectDayBudget();
-
-function detectLevel() {
-	if(appData.moneyPerDay < 100) {
-		console.log('Минимальный уровень достатка');
-	} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-		console.log('Средний уровень достатка');
-	} else if (appData.moneyPerDay > 2000) {
-		console.log('Высокий уровень достатка');
-	} else {
-		console.log('Произошла ошибка');
-	}
-}
-
-detectLevel();
-
-function checkSavings() {
-	if (appData.savings == true) {
-		let save = +prompt('Какова сумма накоплений?', '25000'),
-				percent = +prompt('Под какой процент?', '7');
-
-				appData.monthIncome = (save / 100 / 12 * percent).toFixed();
-				alert(`Доход в месяц с вашего депозита: ${appData.monthIncome} рублей`);
-	}
-}
-
-checkSavings();
-
-function chooseOptExpenses() {
-	for (let i = 1; i < 4; i++) {
-		let question = +prompt('Статья необязательных расходов?', '10000');
-
-		appData.optionalExpenses[i] = question;
-	}
-}
-
-chooseOptExpenses();
